e6ab308663380e1a02482ada04eea08261966fb9
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-09 08:28:06 +0100

    feat(user): Setup Email Address Validator

--- src/validators/mail.validator.ts
+++ src/validators/mail.validator.ts
@@ -1,6 +1,7 @@
 import {registerDecorator, ValidationOptions} from 'class-validator';
 import {RoleType} from "../constants";
 
+
 export function IsValidEmail(role: RoleType, validationOptions?: ValidationOptions) {
   return function (object: object, propertyName: string) {
     registerDecorator({
@@ -11,18 +12,13 @@ export function IsValidEmail(role: RoleType, validationOptions?: ValidationOptio
       options: validationOptions,
       validator: {
         validate(value: string) {
-          let emailRegex;
+          let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
           if (role in [RoleType.ADMIN, RoleType.STAFF]) {
             emailRegex = /^[a-zA-Z]+@futa\.edu\.ng$/;
-          } else if (RoleType.STUDENT) {
+          } else if (role === RoleType.STUDENT) {
             emailRegex = /^[a-zA-Z]{1,}[a-zA-Z]{3}\d{6}@futa\.edu\.ng$/;
-          } else {
-            emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
           }
           return emailRegex.test(value);
-        },
-        defaultMessage() {
-          return '$property is not a valid staff mail';
         },
       },
     });
