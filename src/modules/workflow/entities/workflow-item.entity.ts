4c51c718b74ff3c205263f1143eaf9777eac20fb
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-14 11:07:10 +0100

    feat(workflow): Add group to workflow items

--- src/modules/workflow/entities/workflow-item.entity.ts
+++ src/modules/workflow/entities/workflow-item.entity.ts
@@ -1,8 +1,9 @@
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne } from 'typeorm';
 
 import type { IAbstractEntity } from '../../../common/abstract.entity';
 import { AbstractEntity } from '../../../common/abstract.entity';
 import { UseDto } from '../../../decorators';
+import { GroupEntity } from '../../group/entities/group.entity';
 import type { WorkflowItemDtoOptions } from '../dto/workflow-item.dto';
 import { WorkflowItemDto } from '../dto/workflow-item.dto';
 import { WorkflowEntity } from './workflow.entity';
@@ -26,6 +27,13 @@ export class WorkflowItemEntity
 
   @Column({ type: 'uuid', nullable: false })
   workflowId: Uuid;
+
+  @Column({ type: 'uuid', nullable: false })
+  groupId: Uuid;
+
+  @OneToOne(() => GroupEntity)
+  @JoinColumn({ name: 'group_id' })
+  group: GroupEntity;
 
   @ManyToOne(
     () => WorkflowEntity,
