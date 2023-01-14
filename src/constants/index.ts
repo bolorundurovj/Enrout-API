419fd1bf02826c41b5056ad08497cc8054cf84c7
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-14 21:11:11 +0100

    feat(staff): Add staff designation enum

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,5 +2,6 @@ export * from './division-type';
 export * from './language-code';
 export * from './order';
 export * from './role-type';
+export * from './staff-designation';
 export * from './student-role-type';
 export * from './token-type';
