183954738313088818fa8aa2b1ce064eb7c56c1b
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-25 17:02:42 +0100

    feat(documents): Setup Docs extended CRUD repo functionality

--- src/modules/document/document.service.ts
+++ src/modules/document/document.service.ts
@@ -5,6 +5,7 @@ import { Transactional } from 'typeorm-transactional-cls-hooked';
 
 import type { PageDto } from '../../common/dto/page.dto';
 import type { PageOptionsDto } from '../../common/dto/page-options.dto';
+import { DocumentState } from '../../constants';
 import { CreateDocumentDto } from './dto/create-document.dto';
 import type { DocumentDto } from './dto/document.dto';
 import type { UpdateDocumentDto } from './dto/update-document.dto';
@@ -18,6 +19,12 @@ export class DocumentService {
   ) {}
 
   @Transactional()
+  /**
+   * It creates a new document and saves it to the database
+   * @param {Uuid} ownerId - Uuid - The ownerId is the id of the user who created the document.
+   * @param {CreateDocumentDto} createDocumentDto - CreateDocumentDto
+   * @returns A promise of a DocumentEntity
+   */
   async create(
     ownerId: Uuid,
     createDocumentDto: CreateDocumentDto,
@@ -31,6 +38,11 @@ export class DocumentService {
     return doc;
   }
 
+  /**
+   * It returns a paginated list of documents, with their owners, currently assigned users and workflows
+   * @param {PageOptionsDto} pageOptionsDto - This is the object that contains the page number and the page size.
+   * @returns An array of items and a pageMetaDto
+   */
   async findAll(pageOptionsDto: PageOptionsDto): Promise<PageDto<DocumentDto>> {
     const queryBuilder = this.docRepository
       .createQueryBuilder('doc')
@@ -57,6 +69,11 @@ export class DocumentService {
     return items.toPageDto(pageMetaDto);
   }
 
+  /**
+   * It finds a document by its id, and returns the document with its owner, currentlyAssigned, and workflow
+   * @param {Uuid} id - Uuid - The id of the document to be retrieved
+   * @returns A document entity
+   */
   async findOne(id: Uuid): Promise<DocumentEntity> {
     const queryBuilder = this.docRepository
       .createQueryBuilder('doc')
@@ -89,6 +106,14 @@ export class DocumentService {
     return docEntity;
   }
 
+  /**
+   * We're using the `createQueryBuilder` method to create a query builder object. We're then using the `where` method to
+   * add a `where` clause to the query. We're then using the `getOne` method to execute the query and return the first
+   * result
+   * @param {Uuid} id - Uuid - The id of the document to update
+   * @param {UpdateDocumentDto} updateDocumentDto - UpdateDocumentDto
+   * @returns The updated document
+   */
   async update(
     id: Uuid,
     updateDocumentDto: UpdateDocumentDto,
@@ -108,6 +133,13 @@ export class DocumentService {
     return docEntity;
   }
 
+  /**
+   * We're using the `createQueryBuilder` method to create a query builder object, which we can use to build a query. We're
+   * using the `where` method to add a `where` clause to the query. We're using the `getOne` method to execute the query
+   * and return the first result
+   * @param {Uuid} id - Uuid - The id of the document to be deleted.
+   * @returns The docEntity is being returned.
+   */
   async remove(id: Uuid) {
     const queryBuilder = this.docRepository
       .createQueryBuilder('doc')
@@ -120,6 +152,209 @@ export class DocumentService {
     }
 
     await this.docRepository.remove(docEntity);
+
+    return docEntity;
+  }
+
+  /**
+   * > This function will update the state of a document to REJECTED
+   * @param {Uuid} id - Uuid - the id of the document to reject
+   * @returns DocumentEntity
+   */
+  async rejectDocument(id: Uuid): Promise<DocumentEntity> {
+    const queryBuilder = this.docRepository
+      .createQueryBuilder('doc')
+      .where('doc.id = :id', { id });
+
+    const docEntity = await queryBuilder.getOne();
+
+    if (!docEntity) {
+      throw new NotFoundException();
+    }
+
+    await this.docRepository.update({ id }, { state: DocumentState.REJECTED });
+
+    return docEntity;
+  }
+
+  /**
+   * It takes a staffId and a docId, and then it updates the document with the given docId to have the given staffId as the
+   * currentlyAssignedId
+   * @param {Uuid} staffId - Uuid - The staff member's ID
+   * @param {Uuid} docId - Uuid - The id of the document to be forwarded
+   * @returns DocumentEntity
+   */
+  async forwardDocument(staffId: Uuid, docId: Uuid): Promise<DocumentEntity> {
+    const queryBuilder = this.docRepository
+      .createQueryBuilder('doc')
+      .where('doc.id = :id', { id: docId });
+
+    const docEntity = await queryBuilder.getOne();
+
+    if (!docEntity) {
+      throw new NotFoundException();
+    }
+
+    await this.docRepository.update(
+      { id: docId },
+      { currentlyAssignedId: staffId },
+    );
+
+    return docEntity;
+  }
+
+  /**
+   * It deletes a document by setting the isDeleted flag to true
+   * @param {Uuid} studentId - Uuid - the student's id
+   * @param {Uuid} docId - Uuid - the id of the document to be deleted
+   * @returns The document entity that was deleted.
+   */
+  async studentDeleteDocument(
+    studentId: Uuid,
+    docId: Uuid,
+  ): Promise<DocumentEntity> {
+    const queryBuilder = this.docRepository
+      .createQueryBuilder('doc')
+      .where('doc.id = :id', { id: docId })
+      .andWhere('doc.ownerId = :id', { id: studentId });
+
+    const docEntity = await queryBuilder.getOne();
+
+    if (!docEntity) {
+      throw new NotFoundException();
+    }
+
+    await this.docRepository.update({ id: docId }, { isDeleted: true });
+
+    return docEntity;
+  }
+
+  /**
+   * It takes in a studentId, a staffId, and a docId, and returns a DocumentEntity
+   * @param {Uuid} studentId - The id of the student who owns the document
+   * @param staffId - The id of the staff member who is assigned to the document
+   * @param Uuid - A type that represents a UUID.
+   * @param {Uuid} docId - Uuid - the id of the document that is being published
+   * @returns DocumentEntity
+   */
+  async publishDocument(
+    studentId: Uuid,
+    staffId,
+    Uuid,
+    docId: Uuid,
+  ): Promise<DocumentEntity> {
+    const queryBuilder = this.docRepository
+      .createQueryBuilder('doc')
+      .where('doc.ownerId = :id', { id: studentId })
+      .andWhere('doc.id = :id', { id: docId });
+
+    const docEntity = await queryBuilder.getOne();
+
+    if (!docEntity) {
+      throw new NotFoundException();
+    }
+
+    await this.docRepository.update(
+      { id: docId },
+      { currentlyAssignedId: staffId },
+    );
+
+    return docEntity;
+  }
+
+  /**
+   * It returns a page of student documents for a given student id
+   * @param {Uuid} id - Uuid - the id of the student
+   * @param {PageOptionsDto} pageOptionsDto - This is a class that contains the page number and the page size.
+   * @returns A page of student documents.
+   */
+  async findAllStudentDocs(
+    id: Uuid,
+    pageOptionsDto: PageOptionsDto,
+  ): Promise<PageDto<DocumentDto>> {
+    const queryBuilder = this.docRepository
+      .createQueryBuilder('doc')
+      .where('doc.id = :id', { id })
+      .where('doc.isDeleted = :bool', { bool: false });
+    const [items, pageMetaDto] = await queryBuilder.paginate(pageOptionsDto);
+
+    return items.toPageDto(pageMetaDto);
+  }
+
+  /**
+   * It returns a page of students who are currently assigned to the staff member with the given id
+   * @param {Uuid} id - Uuid - the id of the staff member
+   * @param {PageOptionsDto} pageOptionsDto - This is a class that contains the page number and the page size.
+   * @returns A page of students that are currently assigned to the staff member with the given id.
+   */
+  async findStaffAssignedDocs(
+    id: Uuid,
+    pageOptionsDto: PageOptionsDto,
+  ): Promise<PageDto<DocumentDto>> {
+    const queryBuilder = this.docRepository
+      .createQueryBuilder('doc')
+      .where('doc.currentlyAssigned = :id', { id })
+      .where('doc.isDeleted = :bool', { bool: false });
+    const [items, pageMetaDto] = await queryBuilder.paginate(pageOptionsDto);
+
+    return items.toPageDto(pageMetaDto);
+  }
+
+  /**
+   * It updates a document that is assigned to a staff member
+   * @param {Uuid} staffId - Uuid - The id of the staff member who is currently assigned to the document
+   * @param {Uuid} docId - Uuid,
+   * @param {UpdateDocumentDto} updateDocumentDto - UpdateDocumentDto
+   * @returns The updated document entity.
+   */
+  async updateStaffAssignedDoc(
+    staffId: Uuid,
+    docId: Uuid,
+    updateDocumentDto: UpdateDocumentDto,
+  ): Promise<DocumentEntity> {
+    const queryBuilder = this.docRepository
+      .createQueryBuilder('doc')
+      .where('doc.currentlyAssignedId = :id', { id: staffId })
+      .andWhere('doc.id = :id', { id: docId });
+
+    const docEntity = await queryBuilder.getOne();
+
+    if (!docEntity) {
+      throw new NotFoundException();
+    }
+
+    await this.docRepository.update({ id: docId }, updateDocumentDto);
+
+    return docEntity;
+  }
+
+  /**
+   * "Update a document owned by a student."
+   *
+   * The first thing we do is create a query builder. We use the query builder to find the document we want to update. We
+   * use the `andWhere` method to make sure the document we're updating is owned by the student
+   * @param {Uuid} studentId - Uuid - The student's ID
+   * @param {Uuid} docId - Uuid,
+   * @param {UpdateDocumentDto} updateDocumentDto - UpdateDocumentDto
+   * @returns The updated document entity.
+   */
+  async studentUpdateDoc(
+    studentId: Uuid,
+    docId: Uuid,
+    updateDocumentDto: UpdateDocumentDto,
+  ): Promise<DocumentEntity> {
+    const queryBuilder = this.docRepository
+      .createQueryBuilder('doc')
+      .where('doc.ownerId = :id', { id: studentId })
+      .andWhere('doc.id = :id', { id: docId });
+
+    const docEntity = await queryBuilder.getOne();
+
+    if (!docEntity) {
+      throw new NotFoundException();
+    }
+
+    await this.docRepository.update({ id: docId }, updateDocumentDto);
 
     return docEntity;
   }
