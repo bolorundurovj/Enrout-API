95d53f08191d764ce66d9b30767813c6c80b1c71
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-25 16:57:46 +0100

    feat(students): Setup Student CRUD Service

--- src/modules/student/student.service.ts
+++ src/modules/student/student.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import type { FindOptionsWhere } from 'typeorm';
 import { Repository } from 'typeorm';
 import { Transactional } from 'typeorm-transactional-cls-hooked';
 
+import type { PageDto } from '../../common/dto/page.dto';
+import type { PageOptionsDto } from '../../common/dto/page-options.dto';
 import { FileNotImageException } from '../../exceptions';
 import { IFile } from '../../interfaces';
 import { AwsS3Service } from '../../shared/services/aws-s3.service';
@@ -24,6 +26,12 @@ export class StudentService {
   ) {}
 
   @Transactional()
+  /**
+   * It creates a new student, uploads the avatar to AWS S3, and saves the student to the database
+   * @param {CreateStudentDto} createStudentDto - CreateStudentDto - This is the DTO that we created earlier.
+   * @param {IFile} [file] - IFile - this is the file that was uploaded by the user.
+   * @returns The student entity
+   */
   async create(
     createStudentDto: CreateStudentDto,
     file?: IFile,
@@ -43,31 +51,105 @@ export class StudentService {
     return student;
   }
 
-  findAll() {
-    return `This action returns all student`;
+  /**
+   * It takes a PageOptionsDto object as an argument, and returns a PageDto object
+   * @param {PageOptionsDto} pageOptionsDto - This is a class that contains the page number and the page size.
+   * @returns A PageDto<StudentDto>
+   */
+  async findAll(pageOptionsDto: PageOptionsDto): Promise<PageDto<StudentDto>> {
+    const queryBuilder = this.studentRepository.createQueryBuilder('student');
+    const [items, pageMetaDto] = await queryBuilder.paginate(pageOptionsDto);
+
+    return items.toPageDto(pageMetaDto);
   }
 
+  /**
+   * It returns a promise of a student entity or null
+   * @param findData - FindOptionsWhere<StudentEntity>
+   * @returns Promise<StudentEntity | null>
+   */
   findOne(
     findData: FindOptionsWhere<StudentEntity>,
   ): Promise<StudentEntity | null> {
     return this.studentRepository.findOneBy(findData);
   }
 
-  findById(id: number) {
-    return `This action gets a #${id} student`;
+  /**
+   * It creates a query builder, adds a where clause to it, and then executes the query
+   * @param {Uuid} id - Uuid - The id of the student we want to find.
+   * @returns StudentEntity
+   */
+  async findById(id: Uuid): Promise<StudentEntity> {
+    const queryBuilder = this.studentRepository
+      .createQueryBuilder('student')
+      .where('student.id = :id', { id });
+
+    const studentEntity = await queryBuilder.getOne();
+
+    if (!studentEntity) {
+      throw new NotFoundException();
+    }
+
+    return studentEntity;
   }
 
-  update(id: number, updateStudentDto: UpdateStudentDto) {
-    return {
-      id,
-      student: updateStudentDto,
-    };
+  /**
+   * We're using the `createQueryBuilder` method to create a query builder object, which we can use to build a query. We're
+   * using the `where` method to add a `where` clause to the query. We're using the `getOne` method to execute the query
+   * and return the first result
+   * @param {Uuid} id - Uuid - The id of the student we want to update.
+   * @param {UpdateStudentDto} updateStudentDto - UpdateStudentDto
+   * @returns The studentEntity is being returned.
+   */
+  async update(
+    id: Uuid,
+    updateStudentDto: UpdateStudentDto,
+  ): Promise<StudentEntity> {
+    const queryBuilder = this.studentRepository
+      .createQueryBuilder('student')
+      .where('student.id = :id', { id });
+
+    const studentEntity = await queryBuilder.getOne();
+
+    if (!studentEntity) {
+      throw new NotFoundException();
+    }
+
+    await this.studentRepository.update({ id }, updateStudentDto);
+
+    return studentEntity;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} student`;
+  /**
+   * We're using the `createQueryBuilder` method to create a query builder object, which we can use to build a query. We're
+   * using the `where` method to add a `where` clause to the query. We're using the `getOne` method to execute the query
+   * and return the first result
+   * @param {Uuid} id - Uuid - The id of the student to be deleted
+   * @returns The studentEntity is being returned.
+   */
+  async remove(id: Uuid) {
+    const queryBuilder = this.studentRepository
+      .createQueryBuilder('student')
+      .where('student.id = :id', { id });
+
+    const studentEntity = await queryBuilder.getOne();
+
+    if (!studentEntity) {
+      throw new NotFoundException();
+    }
+
+    await this.studentRepository.remove(studentEntity);
+
+    return studentEntity;
   }
 
+  /**
+   * It takes a user, a hash, and a tokenExpiry, and then updates the user's token and tokenExpiry in the database
+   * @param {StudentEntity} user - StudentEntity - The user entity that we want to save the token to.
+   * @param {string} hash - The hashed token that will be saved in the database.
+   * @param {Date} tokenExpiry - Date,
+   * @returns The userEntity.raw is being returned.
+   */
   async saveToken(
     user: StudentEntity,
     hash: string,
@@ -82,6 +164,12 @@ export class StudentService {
     return userEntity.raw;
   }
 
+  /**
+   * It takes a user and a password, and updates the user's password
+   * @param {StudentEntity} user - StudentEntity - this is the user that we're updating
+   * @param {string} password - string - The password that the user wants to set.
+   * @returns UserDto
+   */
   async savePassword(user: StudentEntity, password: string): Promise<UserDto> {
     const userEntity = await this.studentRepository.update(
       { id: user.id },
