473572fe5c8181affede219dff0b34ffc24df645
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-25 17:58:14 +0100

    feat(documents): Setup Staff Documents Endpoints

--- src/modules/staff/staff.controller.ts
+++ src/modules/staff/staff.controller.ts
@@ -11,16 +11,25 @@ import { ApiTags } from '@nestjs/swagger';
 
 import type { PageDto } from '../../common/dto/page.dto';
 import { PageOptionsDto } from '../../common/dto/page-options.dto';
-import { UUIDParam } from '../../decorators';
+import { AuthUser, UUIDParam } from '../../decorators';
+import { DocumentService } from '../document/document.service';
+import type { DocumentDto } from '../document/dto/document.dto';
+import { UpdateDocumentDto } from '../document/dto/update-document.dto';
+import { StudentEntity } from '../student/entities/student.entity';
 import { CreateStaffDto } from './dto/create-staff.dto';
+import { ForwardDocumentDto } from './dto/forward-document.dto';
 import type { StaffDto } from './dto/staff.dto';
 import { UpdateStaffDto } from './dto/update-staff.dto';
+import type { StaffEntity } from './entities/staff.entity';
 import { StaffService } from './staff.service';
 
 @Controller('staff')
 @ApiTags('Staff')
 export class StaffController {
-  constructor(private readonly staffService: StaffService) {}
+  constructor(
+    private readonly staffService: StaffService,
+    private readonly documentService: DocumentService,
+  ) {}
 
   @Post()
   async create(@Body() createStaffDto: CreateStaffDto) {
@@ -58,5 +67,63 @@ export class StaffController {
     const staffEntity = await this.staffService.remove(id);
 
     return staffEntity.toDto({ isActive: true });
+  }
+
+  @Get('/documents')
+  async findAllDocs(
+    @Query() pageOptionsDto: PageOptionsDto,
+    @AuthUser() user: StudentEntity,
+  ): Promise<PageDto<DocumentDto>> {
+    return this.documentService.findStaffAssignedDocs(user.id, pageOptionsDto);
+  }
+
+  @Get('/documents/:id')
+  async findOneDoc(
+    @UUIDParam('id') id: Uuid,
+    @AuthUser() user: StudentEntity | StaffEntity,
+  ): Promise<DocumentDto> {
+    const docEntity = await this.documentService.staffFindOne(user.id, id);
+
+    return docEntity.toDto();
+  }
+
+  @Patch('documents/:id')
+  async updateDOc(
+    @UUIDParam('id') id: Uuid,
+    @Body() updateDocumentDto: UpdateDocumentDto,
+    @AuthUser() user: StudentEntity | StaffEntity,
+  ): Promise<DocumentDto> {
+    const docEntity = await this.documentService.updateStaffAssignedDoc(
+      user.id,
+      id,
+      updateDocumentDto,
+    );
+
+    return docEntity.toDto();
+  }
+
+  @Patch('documents/:id/forward')
+  async forwardDoc(
+    @UUIDParam('id') id: Uuid,
+    @Body() body: ForwardDocumentDto,
+    @AuthUser() user: StudentEntity | StaffEntity,
+  ): Promise<DocumentDto> {
+    const docEntity = await this.documentService.forwardDocument(
+      user.id,
+      body.staffId,
+      id,
+    );
+
+    return docEntity.toDto();
+  }
+
+  @Patch('documents/:id/reject')
+  async rejectDoc(
+    @UUIDParam('id') id: Uuid,
+    @AuthUser() user: StudentEntity | StaffEntity,
+  ): Promise<DocumentDto> {
+    const docEntity = await this.documentService.rejectDocument(user.id, id);
+
+    return docEntity.toDto();
   }
 }
--- src/modules/staff/staff.module.ts
+++ src/modules/staff/staff.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { DocumentModule } from '../document/document.module';
 import { StaffEntity } from './entities/staff.entity';
 import { StaffController } from './staff.controller';
 import { StaffService } from './staff.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StaffEntity])],
+  imports: [TypeOrmModule.forFeature([StaffEntity]), DocumentModule],
   controllers: [StaffController],
   providers: [StaffService],
   exports: [StaffService],
