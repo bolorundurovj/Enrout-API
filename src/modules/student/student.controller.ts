a59cd9b06d2e5f8c6af3e9c5bea1c4d0d4b19272
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-25 18:04:35 +0100

    feat(documents): Setup Student Documents Endpoints

--- src/modules/student/student.controller.ts
+++ src/modules/student/student.controller.ts
@@ -11,16 +11,27 @@ import { ApiTags } from '@nestjs/swagger';
 
 import type { PageDto } from '../../common/dto/page.dto';
 import { PageOptionsDto } from '../../common/dto/page-options.dto';
-import { UUIDParam } from '../../decorators';
+import { RoleType } from '../../constants';
+import { Auth, AuthUser, UUIDParam } from '../../decorators';
+import { DocumentService } from '../document/document.service';
+import { CreateDocumentDto } from '../document/dto/create-document.dto';
+import type { DocumentDto } from '../document/dto/document.dto';
+import { UpdateDocumentDto } from '../document/dto/update-document.dto';
+import type { StaffEntity } from '../staff/entities/staff.entity';
 import { CreateStudentDto } from './dto/create-student.dto';
 import type { StudentDto } from './dto/student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
+import { StudentEntity } from './entities/student.entity';
 import { StudentService } from './student.service';
+import {ForwardDocumentDto} from "../staff/dto/forward-document.dto";
 
 @Controller('students')
 @ApiTags('Students')
 export class StudentController {
-  constructor(private readonly studentService: StudentService) {}
+  constructor(
+    private readonly studentService: StudentService,
+    private readonly documentService: DocumentService,
+  ) {}
 
   @Post()
   async create(@Body() createStudentDto: CreateStudentDto) {
@@ -61,5 +72,80 @@ export class StudentController {
     const studentEntity = await this.studentService.remove(id);
 
     return studentEntity.toDto();
+  }
+
+  @Post('documents')
+  @Auth([RoleType.STUDENT])
+  async createDocument(
+    @Body() createDocumentDto: CreateDocumentDto,
+    @AuthUser() user: StudentEntity | StaffEntity,
+  ) {
+    const docEntity = await this.documentService.create(
+      user.id,
+      createDocumentDto,
+    );
+
+    return docEntity.toDto();
+  }
+
+  @Get('/documents')
+  async findAllDocs(
+    @Query() pageOptionsDto: PageOptionsDto,
+    @AuthUser() user: StudentEntity,
+  ): Promise<PageDto<DocumentDto>> {
+    return this.documentService.findAllStudentDocs(user.id, pageOptionsDto);
+  }
+
+  @Get('/documents/:id')
+  async findOneDoc(
+    @UUIDParam('id') id: Uuid,
+    @AuthUser() user: StudentEntity | StaffEntity,
+  ): Promise<DocumentDto> {
+    const docEntity = await this.documentService.studentFindOne(user.id, id);
+
+    return docEntity.toDto();
+  }
+
+  @Patch('documents/:id')
+  async updateDOc(
+    @UUIDParam('id') id: Uuid,
+    @Body() updateDocumentDto: UpdateDocumentDto,
+    @AuthUser() user: StudentEntity | StaffEntity,
+  ): Promise<DocumentDto> {
+    const docEntity = await this.documentService.studentUpdateDoc(
+      user.id,
+      id,
+      updateDocumentDto,
+    );
+
+    return docEntity.toDto();
+  }
+
+  @Delete('documents/:id')
+  async removeDoc(
+    @UUIDParam('id') id: Uuid,
+    @AuthUser() user: StudentEntity | StaffEntity,
+  ): Promise<DocumentDto> {
+    const docEntity = await this.documentService.studentDeleteDocument(
+      user.id,
+      id,
+    );
+
+    return docEntity.toDto();
+  }
+
+  @Patch('documents/:id/publish')
+  async forwardDoc(
+    @UUIDParam('id') id: Uuid,
+    @Body() body: ForwardDocumentDto,
+    @AuthUser() user: StudentEntity | StaffEntity,
+  ): Promise<DocumentDto> {
+    const docEntity = await this.documentService.publishDocument(
+      user.id,
+      body.staffId,
+      id,
+    );
+
+    return docEntity.toDto();
   }
 }
--- src/modules/student/student.module.ts
+++ src/modules/student/student.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { DocumentModule } from '../document/document.module';
 import { StudentEntity } from './entities/student.entity';
 import { StudentController } from './student.controller';
 import { StudentService } from './student.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StudentEntity])],
+  imports: [TypeOrmModule.forFeature([StudentEntity]), DocumentModule],
   controllers: [StudentController],
   providers: [StudentService],
   exports: [StudentService],
