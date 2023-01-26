657bccee8867d4e426d810b4d5f7e6760eb0a8a4
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-26 18:46:05 +0100

    feat(mail): Add methods to send nudge and document asignment mails

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -14,6 +14,10 @@ export class MailService {
     private configService: ConfigService,
   ) {}
 
+  /**
+   * It sends an email to the user with a link to reset their password
+   * @param mailData - IMailData<{ hash: string; expires: number }>
+   */
   async forgotPassword(mailData: IMailData<{ hash: string; expires: number }>) {
     await this.mailerService.sendMail({
       to: mailData.to,
@@ -33,6 +37,46 @@ export class MailService {
         text2: 'reset-password.text2',
         text3: 'reset-password.text3',
         text4: 'reset-password.text4',
+      },
+    });
+  }
+
+  /**
+   * It sends an email to the user with the email address provided in the mailData object
+   * @param mailData - IMailData<{ name: string; docTitle: string }>
+   */
+  async forwardedDocument(
+    mailData: IMailData<{ name: string; docTitle: string }>,
+  ) {
+    await this.mailerService.sendMail({
+      to: mailData.to,
+      subject: 'Document Submission',
+      template: 'assignment',
+      context: {
+        title: 'A Document Needs Your Review',
+        actionTitle: 'A Document Needs Your Review',
+        app_name: this.configService.get('app.name'),
+        text1: `${mailData.data.name}`,
+        text2: `${mailData.data.docTitle}`,
+      },
+    });
+  }
+
+  /**
+   * It sends an email to the user with the email address in the `to` property of the `mailData` object
+   * @param mailData - IMailData<{ name: string; docTitle: string }>
+   */
+  async sendNudge(mailData: IMailData<{ name: string; docTitle: string }>) {
+    await this.mailerService.sendMail({
+      to: mailData.to,
+      subject: 'Document Review Pending',
+      template: 'assignment',
+      context: {
+        title: 'A Document Is Still Awaiting Your Review',
+        actionTitle: 'A Document Needs Your Review',
+        app_name: this.configService.get('app.name'),
+        text1: `${mailData.data.name}`,
+        text2: `${mailData.data.docTitle}`,
       },
     });
   }
